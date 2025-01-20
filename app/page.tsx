7b6d9e9f5946fc5a8b72159c82459c94d2850e55
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2025-01-20 19:20:49 +0600

    Add project showcase and testimonial sections to Home page; introduce text-outline utility class

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,32 @@ import Button from "@/components/common/Button";
 import Image from "next/image";
 
 export default function Home() {
+  const projects = [
+    {
+      name: 'Brand Journey Improvements',
+      image: '/image/Project-1.png',
+      client: 'Organc',
+      category: ['Branding', 'Logo design']
+    },
+    {
+      name: 'Brand Grouping',
+      image: '/image/Project-2.png',
+      client: 'FR',
+      category: ['Branding', 'Logo design']
+    },
+    {
+      name: 'NFT Glimps',
+      image: '/image/Project-3.png',
+      client: 'Rumanda',
+      category: ['NFT Design']
+    },
+    {
+      name: 'Brand Suggestions',
+      image: '/image/Project-4.png',
+      client: 'T3d',
+      category: ['NFT logo']
+    },
+  ]
   return (
     <div className="space-y-40 py-40">
       <div className="flex flex-col space-y-20">
@@ -31,13 +57,58 @@ export default function Home() {
         </div>
       </div>
 
-      <div>
-        <div className="flex flex-col items-center justify-center space-y-10">
+      <div className="space-y-20">
+        <div className="flex flex-col items-center justify-center space-y-20">
           <h2 className="font-bold">My Projects Highlight</h2>
           <Button variant="outlined" className="uppercase tracking-wider mx-auto">
             Explore More
             <Image src={'/icon/arrow-right.png'} width={16} height={10} alt="arrow-right" className="ml-2" />
           </Button>
+        </div>
+
+        <div className="grid grid-cols-2 gap-x-6 gap-y-20">
+          {projects.map((project, index) => (
+            <div key={index} className="space-y-[32px]">
+              <Image src={project?.image} width={560} height={620} alt="Project" />
+              <div className="space-y-[32px]">
+                <div className="flex items-center space-x-[32px]">
+                  <h4 className="font-syne">{project?.name}</h4>
+                  <div className="w-[48px] h-px bg-brand" />
+                </div>
+                <div className="space-y-[8px]">
+                  <div className="flex space-x-[16px] w-fit">
+                    <span className="w-[56px] text-[14px] text-grayscale-60">Client:</span>
+                    <span className="text-[14px] text-grayscale-20">{project?.client}</span>
+                  </div>
+                  <div className="flex space-x-[16px] w-fit">
+                    <span className="w-[56px] text-[14px] text-grayscale-60">Work:</span>
+                    {project?.category.map((cat, index) => (
+                      <span key={index} className="text-[14px] text-grayscale-20">{cat}</span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex gap-10">
+          <div className="space-y-10 w-fit pt-10">
+            <h2 className="font-syne">Testimonial</h2>
+            <p className="text-grayscale-50">
+              “Aaronn was fantastic to work with from start to finish. We were looking for
+              a simple, stand-out logo and he delivered. I tried designing the logo myself
+              thinking I wouldn't need to pay the money for a professional graphic designer
+              but I was very wrong. Working with Aaronn was worth every penny and was
+              surprisingly affordable! I remember him saying simplicity is key to a successful
+              logo and boy he was right. I can't thank Aaronn enough for his effort and
+              professionalism. I would recommend him to anyone looking for a design!”
+            </p>
+            <h4 className="font-syne">-Martin lee</h4>
+          </div>
+          <div className="w-[199px] h-[183px]">
+            <Image src={'/icon/quote.png'} width={199} height={183} alt="quote" className="w-full h-full object-contain" />
+          </div>
         </div>
       </div>
     </div>
